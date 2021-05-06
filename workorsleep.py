00d585d6aab3deefddbc2c141ca9addce94210d4
Sarah dePalo <sarahdepalo99@gmail.com>
2021-05-05 20:18:27 -0400

    made a quick change to workorsleep.py

--- workorsleep.py
+++ workorsleep.py
@@ -1,6 +1,7 @@
-day = int(input("Pick a day (0-6). "))
+day = int(input("Pick a day (0-6): "))
 days_of_the_week= ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-if day <= 6:
-    print("Go to work!!")
+if day <= 5:
+    print(f"Go to work!! It's a {days_of_the_week[day]}!")
 else:
-    print("Sleep in today!")
+    print(f"Sleep in today, it's a {days_of_the_week[day]}!")
+
